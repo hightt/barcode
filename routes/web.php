fa6d5be6436b69416aec0e3863ecb42c90007a44
Konrad Duda <konrad.duda121@gmail.com>
2023-06-15 16:09:33 +0200

    Add /barcode routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\BarcodeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/barcode', Barcodecontroller::class);
+Route::get('/', [Barcodecontroller::class, 'index']);
+Route::post('/generateWebpBarcode', [Barcodecontroller::class, 'generateWebpBarcode']);
